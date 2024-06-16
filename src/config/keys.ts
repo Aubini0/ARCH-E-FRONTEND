e848c4e7e0920c0a1168731f0c11b8376318fde7
Ashikur Rahman Munna <ashikurrahmanmunna3@gmail.com>
2024-06-16 23:16:38 +0600

    feat:hosted url

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,6 +1,6 @@
 const Keys = {
-  API_BASE_URL: "https://api.arche.social/api",
-  SOCKET_BASE_URL: "https://api.arche.social",
+  API_BASE_URL: process.env.API_BASE_URL || "https://api.arche.social/api",
+  SOCKET_BASE_URL: process.env.SOCKET_BASE_URL || "https://api.arche.social",
 };
 
 export default Keys;
