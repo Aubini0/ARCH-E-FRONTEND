c180956123194ad47f5bc0b7dbd4a1a7f723ec5b
code-with-shahzad <malikshahzad454545@gmail.com>
2024-07-28 21:43:15 +0500

    change dark code to white

--- src/components/shared/Query/codeBlock.tsx
+++ src/components/shared/Query/codeBlock.tsx
@@ -1,7 +1,7 @@
 import { CheckIcon, CopyIcon } from "lucide-react";
 import React from "react";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import prism from 'react-syntax-highlighter/dist/cjs/styles/prism/prism';
+import prism from "react-syntax-highlighter/dist/cjs/styles/prism/prism";
 
 const CustomCodeBlock: React.FC<{ children: string; language: string }> = ({
   children,
@@ -14,10 +14,14 @@ const CustomCodeBlock: React.FC<{ children: string; language: string }> = ({
     setIsCopied(true);
     setTimeout(() => setIsCopied(false), 2000);
   };
-
+  
   return (
     <div className="relative">
-      <SyntaxHighlighter language={language} style={prism}>
+      <SyntaxHighlighter
+        className={`${language == "sh" && "text-white"}`}
+        language={language}
+        style={prism}
+      >
         {children}
       </SyntaxHighlighter>
       <button onClick={handleCopy} className="absolute top-0 right-2">
