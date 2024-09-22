f057264f2eb51a51a8c59a6e725ea94b3a08260e
Ashikur Rahman Munna <ashikurrahmanmunna3@gmail.com>
2024-09-22 22:48:04 +0600

    feat:changes

--- src/components/shared/TasksList/Task.tsx
+++ src/components/shared/TasksList/Task.tsx
@@ -1,36 +1,56 @@
 import { Checkbox } from "@/components/ui/checkbox";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import React, { FC } from "react";
+import { cn } from "@/lib/utils";
+import React, { FC, useState } from "react";
 import { MdEdit } from "react-icons/md";
 import { TbAdjustmentsHorizontal } from "react-icons/tb";
 import { TfiTrash } from "react-icons/tfi";
 
 interface ITask {
   text: string;
+  index: number;
+  setEditingTask: React.Dispatch<
+    React.SetStateAction<{
+      id: string;
+      time_from: string;
+      time_to: string;
+      date: string;
+    } | null>
+  >;
 }
 
-const Task: FC<ITask> = ({ text }) => {
+const Task: FC<ITask> = ({ text, index, setEditingTask }) => {
   return (
-    <div className="w-full tasks_cancel_drag bg-secondary px-3 py-1 rounded-xl flex items-center gap-3 cursor-auto">
-      <Checkbox className="border-zinc-500 border-2 data-[state=checked]:bg-green-800 w-[15px] h-[15px]" />
-      <div className="flex-1 flex items-center justify-between">
-        <h5 className="text-xs">{text}</h5>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <div className="text-xl p-1 rounded-full hover:bg-dark-background cursor-pointer text-zinc-500 duration-100">
-              <TbAdjustmentsHorizontal />
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>
-              <MdEdit /> <span className="ml-2">Rename</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <TfiTrash /> <span className="ml-2">Delete</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div className="w-full tasks_cancel_drag bg-secondary px-3 py-1 rounded-xl cursor-auto">
+      <div className=" flex items-center gap-3">
+        <Checkbox className="border-zinc-500 border-2 data-[state=checked]:bg-green-800 w-[15px] h-[15px]" />
+        <div className="flex-1 flex items-center justify-between">
+          <div>
+            <h5 className="text-xs">{text}</h5>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <div className="text-xl p-1 rounded-full hover:bg-dark-background cursor-pointer text-zinc-500 duration-100">
+                <TbAdjustmentsHorizontal />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onClick={() => {
+                  setEditingTask({ date: "", id: "123", time_from: "12:40", time_to: "02:40" });
+                }}
+              >
+                <MdEdit /> <span className="ml-2">Edit</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <TfiTrash /> <span className="ml-2">Delete</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
+      {index !== 1 ? <span className="text-[8px] ml-[28px] text-white -mt-1 block">2:40 PM - 10:40 PM</span> : <div className="w-full h-[8px]"></div>}
     </div>
   );
 };
--- src/components/shared/TasksList/index.tsx
+++ src/components/shared/TasksList/index.tsx
@@ -4,10 +4,22 @@ import { FaPlus } from "react-icons/fa6";
 import Task from "./Task";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { Button } from "@/components/ui/button";
+import { MdClose } from "react-icons/md";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { AnimatedInput } from "@/components/ui/animated-input";
+import { cn } from "@/lib/utils";
+import { TimePicker12 } from "@/components/ui/time-picker/time-picker-12-hour";
 
 const TasksList = () => {
+  const [date, setDate] = useState<Date>();
   const [value, setValue] = useLocalStorage("task_window", { x: 350, y: 50 });
   const [position, setPosition] = useState(value);
+  const [editingTask, setEditingTask] = useState<{
+    id: string;
+    time_from: string;
+    time_to: string;
+    date: string;
+  } | null>(null);
   return (
     <div className="fixed top-0 left-0 w-0 h-0">
       <Draggable
@@ -21,28 +33,51 @@ const TasksList = () => {
         defaultPosition={value}
       >
         <div className="bg-dark-background rounded-xl w-[300px] h-[345px] overflow-hidden cursor-grab">
-          <div className="py-5 flex items-center justify-between px-5">
-            <h4 className="text-white text-base">Daily Tasks</h4>
-            {/* <div className="w-[42px] h-[42px] rounded-full border border-white p-1">
+          {!editingTask?.id && (
+            <>
+              <div className="py-5 flex items-center justify-between px-5">
+                <h4 className="text-white text-base">Daily Tasks</h4>
+                {/* <div className="w-[42px] h-[42px] rounded-full border border-white p-1">
               <div className="w-full h-full flex items-center justify-center bg-white rounded-full text-2xl text-black">
                 <FaPlus />
               </div>
             </div> */}
-            <Button size={"sm"} className="active:translate-y-[1px]">
-              Create Task
-            </Button>
-          </div>
-          <div className="w-full flex flex-col p-5 pt-0">
-            <div className="flex gap-3 items-center">
-              <h6 className="text-white">0/5</h6>
-              <Button className="text-xs px-3 py-0 leading-[10px] h-[30px] !bg-green-800">Completed</Button>
+                <Button size={"sm"} className="active:translate-y-[1px]">
+                  Create Task
+                </Button>
+              </div>
+              <div className="w-full flex flex-col p-5 pt-0">
+                <div className="flex gap-3 items-center">
+                  <h6 className="text-white">0/5</h6>
+                  <Button className="text-xs px-3 py-0 leading-[10px] h-[30px] !bg-green-800">Completed</Button>
+                </div>
+                <div className="mt-8 flex-grow overflow-y-auto max-h-[180px] hide-scrollbar space-y-3">
+                  {new Array(5).fill(0).map((_, index) => (
+                    <Task setEditingTask={setEditingTask} index={index} text="Gather Data" key={index} />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+          {editingTask?.id && (
+            <div className="w-full cursor-auto h-full p-5 relative">
+              <div onClick={() => setEditingTask(null)} className="w-[32px] h-[32px] hover:bg-white/40 cursor-pointer duration-100 flex items-center justify-center rounded-full">
+                <IoMdArrowRoundBack className="text-xl text-white" />
+              </div>
+              <h5 className="text-white mt-2 mb-3 text-lg">Edit task</h5>
+
+              <div>
+                <AnimatedInput autoComplete="off" label="Task Name" type="text" placeholder="Task Description" />
+              </div>
+              <div className="mt-3">
+                <p className={cn("dark:text-[#848585] text-black font-inter text-xs font-medium leading-[16px]")}>Time</p>
+                <TimePicker12 date={date} setDate={setDate} />
+              </div>
+              <div className="mt-3">
+                <TimePicker12 date={date} setDate={setDate} />
+              </div>
             </div>
-            <div className="mt-8 flex-grow overflow-y-auto max-h-[180px] hide-scrollbar space-y-3">
-              {new Array(5).fill(0).map((_, index) => (
-                <Task text="Gather Data" key={index} />
-              ))}
-            </div>
-          </div>
+          )}
         </div>
       </Draggable>
     </div>
